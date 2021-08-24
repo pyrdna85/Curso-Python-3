57df61277b95459bf4af16c83b7d6d23c832e6f1
Rodrigo Andrade <50802366+pyrdna85@users.noreply.github.com>
2021-08-24 16:51:05 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,9 @@
+print('Linha1')
+print('Linha2')
+# Falando sobre o que se trata essa linha
+# Continuando os comentários
+print('Linha3')  # Isso é outro comentário
+print('Linha4')  # Isso é outro comentário
+print('Linha5')
+# Isso é mais um comentário
+print('Linha6')
