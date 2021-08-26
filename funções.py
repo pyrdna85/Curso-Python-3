edfd3f5141e065aac53b150d0d19e0c8053de67c
pyrdna85 <andrade.pro@outlook.com>
2021-08-26 15:41:29 -0300

    alteração

--- funções.py
+++ funções.py
@@ -78,17 +78,21 @@ print(var)
 função for divisível por 5, retorne buzz. Se o parâmetro da função for divisível por 5 e por 3, 
 retorne FizzBuzz, caso contrário, retorne o número enviado.
 
-
 def fb(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return f'fizzbuzz, {n} é divisivel por 3 e 5'
+    if n % 5 == 0:
+        return f'buzz, {n} é divisível por 5'
     if n % 3 == 0:
-        return 'fizz'
-    elif n % 5 == 0:
-        return 'buzz'
-    elif n % 3 == 0 and n % 5 == 0:
-        return 'fizzbuzz'
-    else:
-        return n
-print(fb(10))
+        return f'fizz, {n} é divisivel por 3'
+    return n
+
+from random import randint
+
+for i in range(100):
+    aleatorio = randint(0, 100)
+    print(fb(aleatorio))
+
 """
 
 
